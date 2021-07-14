b829ab690860154d119c370e51a419100f18248b
g00dluckroon2 <cjddl789123@naver.com>
2021-07-14 16:40:38 +0900

    project 1 초안

---
+++ PJT1/src/aboutme/TodayServlet.java
@@ -0,0 +1,45 @@
+package aboutme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public TodayServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		
+		LocalDateTime nowTime = LocalDateTime.now();
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		String formattedTime = nowTime.format(format);
+		
+		out.println("<h2>현재 시간 : ");
+		out.println(formattedTime+"</h2>");
+		
+		out.println("<a href=\"index.html\">메인 화면</a>");
+		
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
