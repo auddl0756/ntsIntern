0d281300e32703d7a324e8a0c00fc8aa2f958a96
g00dluckroon2 <cjddl789123@naver.com>
2021-07-15 16:49:19 +0900

    코디 리뷰 수정 사항 적용

--- aboutme/src/com/nts/intern/TodayServlet.java
+++ aboutme/src/com/nts/intern/TodayServlet.java
@@ -1,4 +1,4 @@
-
+package com.nts.intern;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,25 +16,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public TodayServlet() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		
+
 		out.println("<html>");
-		out.println("<head><title>form</title></head>");
+		out.println("<head><title>form</title>");
+		out.println("<link rel=\"stylesheet\" href=\"css/todayServlet.css\">");
+		out.println("</head>");
 		out.println("<body>");
-		
+
 		LocalDateTime nowTime = LocalDateTime.now();
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 		String formattedTime = nowTime.format(format);
-		
-		out.println("<h2>현재 시간 : "+formattedTime+"</h2>");
-		
+
+		out.println("<h2>현재 시간 <br/> " + formattedTime + "</h2>");
+
 		out.println("<a href=\"index.html\">메인 화면</a>");
 		
 		out.println("</body>");
