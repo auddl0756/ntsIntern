284b3964ff50020f83d3079986a428f0c0c7af35
g00dluckroon2 <cjddl789123@naver.com>
2021-07-28 14:59:48 +0900

    DB 연결 및 productDao findAll 테스트 코드 작성

---
+++ reservation/src/test/java/com/nts/intern/reserve/dao/ProductDaoTest.java
@@ -0,0 +1,34 @@
+package com.nts.intern.reserve.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nts.intern.reserve.config.ApplicationConfig;
+import com.nts.intern.reserve.dto.ProductDto;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationConfig.class)
+public class ProductDaoTest {
+	@Autowired
+	private ProductDao productDao;
+	
+	@Test
+	public void selectAllTest() {
+		assertThat(productDao).isNotNull();
+		List<ProductDto> list=productDao.findAll(0, 5);
+		
+		list.stream().forEach(product->{
+			assertThat(product.getContent()).isNotNull();
+		});
+		
+	}
+
+}
