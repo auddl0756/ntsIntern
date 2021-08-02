6ad79f1cb2f5d112a10b35de3124c799a75e5468
g00dluckroon2 <cjddl789123@naver.com>
2021-08-02 17:38:37 +0900

    조회 메서드에 읽기 전용으로 트랜잭션 설정

--- reservation/src/main/java/com/nts/intern/reserve/service/ProductServiceImpl.java
+++ reservation/src/main/java/com/nts/intern/reserve/service/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nts.intern.reserve.dto.ProductDto;
 import com.nts.intern.reserve.repository.ProductRepository;
@@ -14,21 +15,25 @@ public class ProductServiceImpl implements ProductService {
 	private ProductRepository productRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<ProductDto> findWithPaging(int start, int limit) {
 		return productRepository.findWithPaging(start, limit);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<ProductDto> findWithPagingAndCategory(int start, int limit, int categoryId) {
 		return productRepository.findWithPagingAndCategory(start, limit, categoryId);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public int getSize() {
 		return productRepository.countAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public int getSizeByCategory(int categoryId) {
 		return productRepository.countByCategory(categoryId);
 	}
