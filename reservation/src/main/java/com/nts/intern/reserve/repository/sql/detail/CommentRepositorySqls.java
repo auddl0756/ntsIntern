dec9739021e9bcd7eb9405bd63c647772f2225c3
g00dluckroon2 <cjddl789123@naver.com>
2021-08-13 10:43:13 +0900

    평점 쿼리 불필요한 부분 제거

--- reservation/src/main/java/com/nts/intern/reserve/repository/sql/detail/CommentRepositorySqls.java
+++ reservation/src/main/java/com/nts/intern/reserve/repository/sql/detail/CommentRepositorySqls.java
@@ -51,17 +51,7 @@ public class CommentRepositorySqls {
 	= "SELECT ROUND(AVG(score),2) as averageScore " + 
 		"FROM " + 
 		"(" +
-		"	SELECT rev_com.score as score," + 
-		" 	rev_com.id as commentId," + 
-		"	rev_com.comment as comment," + 
-		"	rev_com.create_date as createDate," + 
-		"	rev_com.modify_date as modifyDate," + 
-		"	rev_info.product_id as productId," + 
-		"	rev_info.reservation_date as reservationDate," + 
-		"	rev_info.reservation_email as reservationEmail," + 
-		"	rev_info.id as reservationInfoId," + 
-		"	rev_info.reservation_name as reservationName," + 
-		"	rev_info.reservation_tel as reservationTelephone " + 
+		"	SELECT rev_com.score as score " + 
 		"	FROM reservation_info rev_info " + 
 		"	INNER JOIN reservation_user_comment rev_com on rev_com.reservation_info_id = rev_info.id " + 
 		"	WHERE rev_info.display_info_id  = :displayInfoId " + 
